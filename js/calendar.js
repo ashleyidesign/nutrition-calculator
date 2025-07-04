cfcf7fab94222a4a60eb8d4492f59a84512ba8e8
Ashley Hill <ashley@ashleyidesign.com>
2025-07-04 08:13:31 -0400

    Update calendar.js

--- js/calendar.js
+++ js/calendar.js
@@ -405,6 +405,11 @@ const calendarManager = {
         });
     },
     
+    isSameDate(date1, date2) {
+        return date1.getFullYear() === date2.getFullYear() &&
+               date1.getMonth() === date2.getMonth() &&
+               date1.getDate() === date2.getDate();
+    },
     
     daysBetween(startDate, endDate) {
         const timeDiff = endDate.getTime() - startDate.getTime();
